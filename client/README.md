50a11da73c60b9d5e7dbbeacc3f4770ea9645258
RinkaKesh <keshrinka2013@gmail.com>
2024-06-19 18:05:52 +0530

    updated

--- client/README.md
+++ client/README.md
@@ -6,3 +6,8 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+
+<li className={style.user_container}><Link to="/login"><IoPersonSharp/>Login</Link></li>
+                <li className={style.cart_container}><Link to="/cart"><BsCart4/>Cart</Link></li>
+                <li className={style.seller_container}><Link><PiGiftBold/>Become a Seller</Link></li>
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { FaBars, FaTimes } from 'react-icons/fa';
 import { IoSearchOutline } from "react-icons/io5";
-import style from "./Navbar.module.css"
+import styles from "./Navbar.module.css"
 import logo from "../../../public/dealmart.png"
 import { IoPersonSharp } from "react-icons/io5";
 import { BsCart4 } from "react-icons/bs";
@@ -8,17 +9,32 @@ import { PiGiftBold } from "react-icons/pi"
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
-        <div className={style.navbar_container}>
-            <ul className={style.Navbar}>
-                <li><img src={logo} alt="logo" className={style.logo} /></li>
-                <li className={style.search_container}><IoSearchOutline className={style.search_icon} /><input type="text" className={style.search_input} placeholder='Search Products...' maxLength={15} /></li>
-                <li className={style.user_container}><Link to="/login"><IoPersonSharp/>Login</Link></li>
-                <li className={style.cart_container}><Link to="/cart"><BsCart4/>Cart</Link></li>
-                <li className={style.seller_container}><Link><PiGiftBold/>Become a Seller</Link></li>
-            </ul>
-        </div>
-    )
-}
+        <nav className={styles.navbar}>
+            <img src="public/dealmart.png" alt="logo" />
+            <div className={styles.menu}>
+                <div>
+                    {menuOpen ? (
+                        <FaTimes className={styles.menuBtn} onClick={() => setMenuOpen(!menuOpen)} />
+                    ) : (
+                        <FaBars className={styles.menuBtn} onClick={() => setMenuOpen(!menuOpen)} />
+                    )}
+                </div>
+                <ul
+                    className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+                    onClick={() => setMenuOpen(false)}
+                >
+                    <li className={styles.user_container}><Link to="/login"><IoPersonSharp />Login</Link></li>
+                    <li className={styles.cart_container}><Link to="/cart"><BsCart4 />Cart</Link></li>
+                    <li className={styles.seller_container}><Link><PiGiftBold />Become a Seller</Link></li>
+                </ul>
+            </div>
+        </nav>
+    );
+};
+
+
 
 export default Navbar
