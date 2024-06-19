068216dcc92af4c66d0d7e61e46719d66f487bfb
RinkaKesh <keshrinka2013@gmail.com>
2024-06-19 22:27:06 +0530

    updated

--- client/src/pages/SignIn/SignIn.jsx
+++ client/src/pages/SignIn/SignIn.jsx
@@ -1,34 +1,31 @@
 import React from 'react';
 import style from "./SignIn.module.css";
 import login from "../../../public/login/login9.jpg";
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export const SignIn = () => {
-  const navigate = useNavigate();
-  const handleLogin = () => {
-    navigate('/product');
-  };
-
   return (
-    <div className={style.signin_root}>
-      <div className={style.signin_container}>
-        <div className={style.left}>
-          <ul className={style.login_text}>Login
+    <div className={style.signin_container}>
+      <div className={style.left}>
+        <div className={style.login_text}>
+          <ul>
+            Login
             <li>Get access to your Orders, <br />Wishlist and Recommendations</li>
           </ul>
-          <img src={login} alt="Login" className={style.login} />
         </div>
-        <div className={style.right}>
-          <form action="">
-            <label htmlFor="email">Enter Email</label><br />
-            <input type="email" id="email" /><br /><br />
-            <label htmlFor="password">Enter Password</label><br />
-            <input type="password" id="password" /><br /><br />
-            <p>By continuing, you agree to Dealmart's <a href="#">Terms of Use</a> and <a href="#">Privacy Policy</a>.</p>
-            <button type="button" onClick={handleLogin}>Login</button>
-          </form>
-          <Link className={style.new_account} to="/register">New to Dealmart? Create an account</Link>
-        </div>
+        <img src={login} alt="Login" />
+      </div>
+      <div className={style.right}>
+        <form>
+          <label htmlFor="email">Enter Email</label>
+          <input type="email" name='email' id='email' />
+           
+          <label htmlFor="password">Enter Password</label>
+          <input type="password" name='password' id='password' />
+          <p>By continuing, you agree to TechNote's <a href="">Terms of Use</a> and <a href="">Privacy Policy</a>.</p>
+          <button type="submit">Login</button>
+        </form> 
+        <Link className={style.new_account} to="/register">New to TechNote? Create an account</Link>
       </div>
     </div>
   );
